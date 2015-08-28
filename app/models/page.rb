edf4d0212d69fb2643dc8d480e58a8644ab53b74
sephirothkod <bjustice@uvic.ca>
2015-08-28 09:50:35 -0700

    Create a content resource

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,6 @@
 class Page < ActiveFedora::Base
 
+	contains "pageContent"
 	property :number, predicate: ::RDF::URI.new('http://opaquenamespace.org/hydra/pageNumber'), multiple: false do |index|
 		index.as :stored_searchable
 		index.type :integer
@@ -10,5 +11,6 @@ class Page < ActiveFedora::Base
 	end
 
 	belongs_to :book, predicate: ActiveFedora::RDF::Fcrepo::RelsExt.isPartOf
+	
 
 end
